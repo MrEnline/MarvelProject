e741a83497b56a7bfd3575727c452ab5508e1f77
Sergey Kuznetsov <mr.enline@gmail.com>
2022-03-04 12:52:16 +0300

    Реализовано формирование листа динамически с персонажами

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -84,7 +84,6 @@ class RandomChar extends Component {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
     let imgStyle = {'objectFit' : 'cover'};
-    console.log(thumbnail);
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
     }
