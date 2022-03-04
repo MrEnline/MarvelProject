ba7532057d51b404150edaeb7ca46b9fbd651ca5
Sergey Kuznetsov <mr.enline@gmail.com>
2022-03-04 11:55:48 +0300

    сделана кнопка try it со спиннером

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -6,16 +6,19 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
-
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
-    
     state = {
         char: {},
         loading: true,
         error: false
+    }
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 15000); Обновление персонажей в верхней части сайта кажды 15 секунд
+    }
+
+    componentWillUnmount() {
+        //clearInterval(this.timerId);
     }
     
     marvelService = new MarvelService();
@@ -23,6 +26,10 @@ class RandomChar extends Component {
     //загрузка данных по персонажу закончена
     onCharLoaded = (char) => {
         this.setState({char, loading: false}); //({char}) идентично ({char: char})
+    }
+
+    onCharLoading = () => {
+        this.setState({loading: true});
     }
 
     onError = () => {
@@ -35,6 +42,7 @@ class RandomChar extends Component {
     updateChar = () => {
         //id от 1011000 до 1011400
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         //в then можно так как ниже или (res => this.onCharLoaded(res))
         this.marvelService
             .getCharacter(id)
@@ -63,7 +71,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -75,9 +83,14 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
+    let imgStyle = {'objectFit' : 'cover'};
+    console.log(thumbnail);
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
