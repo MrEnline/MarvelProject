d6dc02f104e55bf7b75b6aea09517441a01c1823
Kuznetsov Sergey <mr.enline@gmail.com>
2022-03-26 22:38:11 +0300

    Small changes

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -95,8 +95,7 @@ const View = ({char}) => {
             <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
-                <p className="randomchar__descr">
-                    {description}                        </p>
+                <p className="randomchar__descr">{description}</p>
                 <div className="randomchar__btns">
                     <a href={homepage} className="button button__main">
                         <div className="inner">homepage</div>
