1231db6e651e2e294334bb6c725d88bf85d6eb0a
Sergey Kuznetsov <mr.enline@gmail.com>
2022-05-17 18:35:10 +0300

    реализована маршрутизация с помощью React Router v5

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,9 @@
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import SingleComic from "../singleComic/SingleComic";
-
-import decoration from '../../resources/img/vision.png';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppHeader from '../appHeader/AppHeader';
+import { MainPage, ComicsPage } from '../pages';
 
 //import MarvelService from "../../services/MarvelService";
-import { useState } from "react";
+import { useState } from 'react';
 
 //const marvelService = new MarvelService();
 // marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item)));
@@ -17,43 +11,23 @@ import { useState } from "react";
 //marvelService.getAllCharacters().then(res => console.log(res));
 
 const App = () => {
-    
-    const [selectedChar, setChar] = useState(null);
-    const [selectedComics, setComics] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
-    const onComicsSelected = (id) => {
-        setComics(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <ErrorBoundary>
-                    <ComicsList onComicsSelected={onComicsSelected}/>
-                </ErrorBoundary>
-                {/* <ErrorBoundary>
-                    <SingleComic comicsId={selectedComics}/>
-                </ErrorBoundary> */}
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-            </main>
-        </div>
-    )
-}
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage />
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
