02d1416b9be592b812ed45c8619d8be60fef2dd5
Sergey Kuznetsov <mr.enline@gmail.com>
2022-10-05 12:36:44 +0300

    fix bug

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -37,7 +37,7 @@ const App = () => {
                                 <SinglePage Component={SingleCharacterLayout} dataType="character" />
                             </Route> */}
                             <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType="comic" />} />
-                            <Route path="/comics/:id" element={<SinglePage Component={SingleCharacterLayout} dataType="character" />} />
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType="character" />} />
                             <Route path="*" element={<Page404 />} />
                         </Routes>
                     </Suspense>
